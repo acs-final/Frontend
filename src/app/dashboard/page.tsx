92f1338c4e4ecb31ac795c9087e904f891832c6f
wonyoung kim <wy930914@naver.com>
2025-02-20 18:34:46 +0900

    feat:dashboard fix

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -89,36 +89,25 @@ export default function DashboardClient() {
       </div>
       <div className="hidden flex-col md:flex">
         {/* <div className="border-b"> */}
-          <div className="flex h-16 items-center px-4">
+          {/* <div className="flex h-16 items-center px-4"> */}
             {/* <TeamSwitcher /> */}
             {/* <MainNav className="mx-6" /> */}
-            <div className="ml-auto flex items-center space-x-4">
+            {/* <div className="ml-auto flex items-center space-x-4"> */}
               {/* <Search /> */}
               {/* <UserNav /> */}
-            </div>
-          </div>
+            {/* </div> */}
+          {/* </div> */}
         {/* </div> */}
         <div className="flex-1 space-y-4 p-8 pt-6">
-          <div className="flex items-center justify-between space-y-2">
+          {/* <div className="flex items-center justify-between space-y-2"> */}
             <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
             <div className="flex items-center space-x-2">
               {/* <CalendarDateRangePicker /> */}
               {/* <Button>Download</Button> */}
-            </div>
+            {/* </div> */}
           </div>
           <Tabs defaultValue="overview" className="space-y-4">
-            <TabsList>
-              <TabsTrigger value="overview">Overview</TabsTrigger>
-              {/* <TabsTrigger value="analytics" disabled>
-                Analytics
-              </TabsTrigger>
-              <TabsTrigger value="reports" disabled>
-                Reports
-              </TabsTrigger>
-              <TabsTrigger value="notifications" disabled>
-                Notifications
-              </TabsTrigger> */}
-            </TabsList>
+
             <TabsContent value="overview" className="space-y-4">
               <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-3">
                 <Card>
@@ -221,7 +210,7 @@ export default function DashboardClient() {
                 </Card> */}
               </div>
               <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
-                <Card className="col-span-4">
+                <Card className="col-span-5">
                   <CardHeader>
                     <CardTitle>장르별 그래프</CardTitle>
                   </CardHeader>
@@ -244,7 +233,7 @@ export default function DashboardClient() {
                     </ChartContainer>
                   </CardContent>
                 </Card>
-                <Card className="col-span-3">
+                <Card className="col-span-2">
                   <CardHeader>
                     <CardTitle>Top3 생성 동화</CardTitle>
                   </CardHeader>
@@ -254,7 +243,7 @@ export default function DashboardClient() {
                         <li key={index} className="py-1">
                           <Link
                             // href={`/fairytale/${encodeURIComponent(fairytale.title)}`}
-                            href={`/recommended`}
+                            href={`/top`}
                             className="block"
                           >
                             <div className="font-medium">{fairytale.title}</div>
